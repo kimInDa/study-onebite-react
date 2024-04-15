4d38ad06b4dab740baf2fb3076083c0a6c61425b
kimInDa <pinktail2327@naver.com>
2024-04-16 01:11:09 +0900

    [학습] section12 - chapter 08 : 일기 관리 기능 구현하기 2 - 일기 생성, 수정, 삭제 기능 구현
    - useReducer를 활용하여 구현

--- section12-react/src/App.jsx
+++ section12-react/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -24,14 +24,74 @@ const mockData = [
 ];
 
 function reducer(state, action) {
-  return state;
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
 }
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 새로운 일기 추가
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
 
   return (
     <>
+      <button onClick={() => onCreate(new Date().getTime(), 1, "Hello")}>
+        일기 추가 테스트!
+      </button>
+
+      <button
+        onClick={() =>
+          onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다")
+        }
+      >
+        일기 수정 테스트!
+      </button>
+
+      <button onClick={() => onDelete(1)}>일기 삭제 테스트!</button>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
