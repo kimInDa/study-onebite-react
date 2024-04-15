651ae0f516ceb52dde7b66354469c801e0286d92
kimInDa <pinktail2327@naver.com>
2024-04-16 01:17:29 +0900

    [학습] section12 - chapter 08 : 일기 관리 기능 구현하기 2 - Context로 모든 페이지에 일기 데이터와 일기 관리 함수들 공급 설정
    - 일기 데이터 공급 : DiaryStateContext
    - 일기 관리(생성, 수정, 삭제) 함수 공급 : DiaryDispatchContext

--- section12-react/src/App.jsx
+++ section12-react/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useReducer, useRef } from "react";
+import { useReducer, useRef, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
@@ -37,6 +37,9 @@ function reducer(state, action) {
       return state;
   }
 }
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
@@ -92,13 +95,17 @@ function App() {
 
       <button onClick={() => onDelete(1)}>일기 삭제 테스트!</button>
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='/*' element={<Notfound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='/*' element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
