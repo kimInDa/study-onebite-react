2ec90b0d806cab0631d6c805cf99db85adbce889
kimInDa <pinktail2327@naver.com>
2024-04-16 00:16:27 +0900

    [Refactor] [학습] section12 - chapter 07 : 공통 컴포넌트 구현하기 - 실습 전 코드 정리
    - 불필요한 console.log 삭제

--- section12-react/src/pages/Home.jsx
+++ section12-react/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { useSearchParams } from "react-router-dom";
 const Home = () => {
   // # 동적 경로 : Query String 꺼내쓰기
   const [params, setParams] = useSearchParams();
-  console.log(params.get("value"));
 
   return <div>Home</div>;
 };
