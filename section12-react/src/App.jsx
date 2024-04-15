a97999be4524ff0c797fe191a9be2a121f5c7d56
kimInDa <pinktail2327@naver.com>
2024-04-16 00:43:02 +0900

    [학습] section12 - chapter 08 : 일기 관리 기능 구현하기 1 - 실습 전 코드 정리
    - 기능상 불필요한 실습용 코드 및 주석 정리

--- section12-react/src/App.jsx
+++ section12-react/src/App.jsx
@@ -1,52 +1,15 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
 import Edit from "./pages/Edit";
-import Button from "./components/Button";
-import Header from "./components/Header";
-
-// 구현할 페이지 list => page 컴포넌트 생성
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-// 4. "/edit" : 일기를 수정하는 Edit 페이지 (Mission!)
-// 0. "/*" : 설정하지 않은 페이지
 
 function App() {
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("123번 버튼 클릭!");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭!");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("123번 버튼 클릭!");
-        }}
-      />
-
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
