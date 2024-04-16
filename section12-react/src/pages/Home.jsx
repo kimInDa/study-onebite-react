6b658f4b1c6c08f89328ce7af52deef2ee44cc77
kimInDa <pinktail2327@naver.com>
2024-04-16 16:51:24 +0900

    [학습] section12 - chapter 10 : Home 페이지 구현하기 1.UI(Header 컴포넌트 삽입)

--- section12-react/src/pages/Home.jsx
+++ section12-react/src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        title={"2024년 02월"}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+    </div>
+  );
 };
 
 export default Home;
