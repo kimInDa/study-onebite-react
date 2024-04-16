c25063e446eaebd2ad8719b6fcc1339e4efdb948
kimInDa <pinktail2327@naver.com>
2024-04-16 17:05:46 +0900

    [학습] section12 - chapter 10 : Home 페이지 구현하기 1.UI(메뉴바)

--- section12-react/src/components/DiaryList.jsx
+++ section12-react/src/components/DiaryList.jsx
@@ -1,7 +1,16 @@
+import "./DiaryList.css";
+import Button from "./Button";
+
 const DiaryList = () => {
   return (
     <div className='DiaryList'>
-      <div className='menu_bar'></div>
+      <div className='menu_bar'>
+        <select>
+          <option value={"latest"}>최신순</option>
+          <option value={"oldest"}>오래된 순</option>
+        </select>
+        <Button text={"새 일기 쓰기"} type={"POSITIVE"} />
+      </div>
       <div className='list_wrapper'></div>
     </div>
   );
