86c10c221627a1226bce0f56ff585901023a291c
kimInDa <pinktail2327@naver.com>
2024-04-16 17:39:34 +0900

    [학습] section12 - chapter 11 : Home 페이지 구현하기 2.기능(상단바 날짜 변경)
    - 상단바에 좌, 우 버튼 클릭시 상단바 날짜 표기 변경 기능 구현

--- section12-react/src/pages/Home.jsx
+++ section12-react/src/pages/Home.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
+
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 02월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
       <DiaryList />
     </div>
