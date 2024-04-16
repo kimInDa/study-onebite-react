579d81e93e0866b9300e830b91641b718fd8d4df
kimInDa <pinktail2327@naver.com>
2024-04-16 16:39:14 +0900

    [학습] section12 - chapter 10 : 지난 실습 코드 정리
    - 기능상 필요 없는 실습용 코드 제거

--- section12-react/src/pages/Home.jsx
+++ section12-react/src/pages/Home.jsx
@@ -1,9 +1,4 @@
-import { useSearchParams } from "react-router-dom";
-
 const Home = () => {
-  // # 동적 경로 : Query String 꺼내쓰기
-  const [params, setParams] = useSearchParams();
-
   return <div>Home</div>;
 };
 
