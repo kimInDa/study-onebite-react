3a0ad731b231f2a2ef0500f4a7007fa6e50b7681
kimInDa <pinktail2327@naver.com>
2024-04-16 16:55:07 +0900

    [학습] section12 - chapter 10 : Home 페이지 구현하기 1.UI(DiaryList 컴포넌트 생성)

---
+++ section12-react/src/components/DiaryList.jsx
@@ -0,0 +1,10 @@
+const DiaryList = () => {
+  return (
+    <div className='DiaryList'>
+      <div className='menu_bar'></div>
+      <div className='list_wrapper'></div>
+    </div>
+  );
+};
+
+export default DiaryList;
