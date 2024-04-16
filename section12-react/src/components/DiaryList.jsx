1451968a15cebc629d366504dc4be59e13adf72a
kimInDa <pinktail2327@naver.com>
2024-04-16 18:25:47 +0900

    [학습] section12 - chapter 11 : Home 페이지 구현하기 2.기능(다이어리 목록 정렬)
    - 최신순 또는 오래된 순으로 다이어리 목록 정렬 기능 구현

--- section12-react/src/components/DiaryList.jsx
+++ section12-react/src/components/DiaryList.jsx
@@ -1,15 +1,35 @@
 import "./DiaryList.css";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 import DiaryItem from "./DiaryItem";
 
 const DiaryList = ({ data }) => {
   const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedDate = () => {
+    // sort 메서드는 원본 수정되므로 toSorted 메서드 사용하여 원본은 보존!
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  // 컴포넌트가 마운트 될 때마다 새로운 정렬 목록 생성
+  const sortedData = getSortedDate();
 
   return (
     <div className='DiaryList'>
       <div className='menu_bar'>
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={"latest"}>최신순</option>
           <option value={"oldest"}>오래된 순</option>
         </select>
@@ -20,7 +40,7 @@ const DiaryList = ({ data }) => {
         />
       </div>
       <div className='list_wrapper'>
-        {data.map((item) => (
+        {sortedData.map((item) => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
