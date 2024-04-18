e2b408c9e8f451c43c7707dfde4c08143b758d6d
kimInDa <pinktail2327@naver.com>
2024-04-18 17:51:27 +0900

    [학습] section12 - chapter 14 : New페이지 구현하기 2. 기능(일기 추가)
    - New 페이지에서 onCreate 함수를 Editor 컴포넌트로 전달해 주기 위해 onSubmit  함수를 생성 및 전달
    -> Editor 컴포넌트는 New 페이지 뿐만 아니라 Edit 페이지에서도 사용됨. 따라서 각 페이지에서 props로 받는 onSubmit 함수가 다름(New 페이지는 onCreate, Edit 페이지는 onUpdate) 따라서 App.jsx에서 Editor로 바로 함수를 공급받지 말고, Editor 가 사용되는 New나 Edit 페이지에서 공급받는 것이 좋다. 그래야 하나의 Editor 컴포넌트에 하나의 onSubmit 이지만 다르게 동작 할 수 있게 된다.

--- section12-react/src/components/Editor.jsx
+++ section12-react/src/components/Editor.jsx
@@ -28,7 +28,7 @@ const getStringedDate = (targetDate) => {
   return `${year}-${month}-${date}`;
 };
 
-const Editor = () => {
+const Editor = ({ onSubmit }) => {
   const [input, setInput] = useState({
     createdDate: new Date(),
     emotionId: 3,
@@ -48,6 +48,10 @@ const Editor = () => {
       ...input,
       [name]: value,
     });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
   };
 
   return (
@@ -90,7 +94,11 @@ const Editor = () => {
       </section>
       <section className='button_section'>
         <Button text={"취소하기"} />
-        <Button text={"작성하기"} type={"POSITIVE"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성하기"}
+          type={"POSITIVE"}
+        />
       </section>
     </div>
   );
