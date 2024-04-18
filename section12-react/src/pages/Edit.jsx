eef8621961c26ac365032583726eb75332d7452f
kimInDa <pinktail2327@naver.com>
2024-04-18 18:53:04 +0900

    [학습] section12 - chapter 15 : Edit 페이지 구현하기 (일기 삭제 기능)
    - 삭제하기 버튼 클릭 시 확인 기능 구현
    - 삭제하기 버튼 클릭 시 삭제 기능 구현
    - 삭제 확인 시 일기 삭제 후 홈페이지로 이동 + 뒤로가기 금지 기능 구현

--- section12-react/src/pages/Edit.jsx
+++ section12-react/src/pages/Edit.jsx
@@ -1,8 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
   const { id } = useParams();
-  return <div>{id}번 일기입니다</div>;
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      // 일기 삭제 로직
+      onDelete(id);
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
